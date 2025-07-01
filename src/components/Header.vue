4b255abb94cd958bb0aa761b3f0633c647fc6d9a
kaynanDev <kaynan2817@gmail.com>
2025-06-30 22:21:02 -0300

    atualização

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,7 +25,7 @@
           </li>
         </ul>
         <button
-          class="bg-(--color-primary) text-sm duration-200 ease-in hover:opacity-75 hover:cursor-pointer py-2 px-8 rounded-3xl text-(#fff)"
+          class="bg-(--color-primary) text-sm duration-200 ease-in hover:opacity-75 hover:cursor-pointer py-2 px-8 rounded-3xl"
         >
           Baixar o App
         </button>
